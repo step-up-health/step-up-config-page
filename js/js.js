739572c0e8de59906b6484564615ef9283dbc8e4
KEvin Wang <kevinwang@kevinwang.us>
2016-01-31 05:15:48 -0800

    ok

--- js/js.js
+++ js/js.js
@@ -12,6 +12,9 @@ var $submitButton = $('#submitButton');
 $submitButton.on('click', function() {
     console.log('Submit');
     alert("SUBMIT BOTTON");
+
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+    document.location = return_to; //+ encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
 });
 
 
