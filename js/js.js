f81274251ff7ec9f5ae218af1223ce8777cb4c7b
jneubrand <johannes_n@icloud.com>
2016-05-08 17:00:59 +0200

    beautify code; unify quotes

--- js/js.js
+++ js/js.js
@@ -1,101 +1,107 @@
 anchors = window.location.hash.substring(1)
     .split(':');
-uid = anchors[0] || "poop";
+uid = anchors[0] || 'poop';
 timeline = anchors[1];
-// k = $("<div></div");
-// k.text(uid);
-// $("body").append(k);
-base_url = "https://pythonbackend-stepupforpebble.rhcloud.com";
-//alert(window.location.hash);
+base_url = 'https://pythonbackend-stepupforpebble.rhcloud.com';
 var $submitButton = $('#submitButton');
 $submitButton.on('click', function() {
     console.log('Submit');
-    //var return_to = getQueryParam('return_to', 'pebblejs://close#');
-    location.href = 'pebblejs://close#'; //+ encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
+    location.href = 'pebblejs://close#';
 });
-get_friends_error = function() {
+
+function get_friends_error() {
     friends = [];
-    $("#itemlist")
+    $('#itemlist')
         .appendChild($('<label class="item">ERROR FINDING FRIENDS</label>'));
 };
-get_friends_error_hack = function() {
-    friends = ["<i>Donald Glover</i>", "<i>Dave Chappelle</i>", "<i>Louis Szekely</i>", "<i>Aziz Ansari</i>"];
-};
+
 function incoming_button_callback() {
-    sendFriendRequest($(this).parent().data('username'),
-    function() {
-        // success
-        reloadFriends();
-    }, function(err) {
-        alert("Something went wrong—friend request from " + fname + " not accepted.");
-        console.log(err);
-    }
-    );
+    sendFriendRequest($(this)
+        .parent()
+        .data('username'),
+        function() {
+            // success
+            reloadFriends();
+        },
+        function(err) {
+            alert('Something went wrong—friend request from ' + fname + ' not accepted.');
+            console.log(err);
+        });
 }
+
 function set_friends_list() {
-    $("#itemlist").empty();
+    $('#itemlist')
+        .empty();
     for (i = 0; i < friends.length; i++) {
         newitem = $('<label class="item"></label>');
         newitem.text(friends[i]);
-        $("#itemlist")
+        $('#itemlist')
             .append(newitem);
     }
     if (friends.length === 0) {
         newitem = $('<div>You don\'t have any friends yet.</div>');
-        $("#itemlist")
+        $('#itemlist')
             .append(newitem);
     }
     $('#itemlist')
         .itemFriendList();
 }
+
 function set_outgoing_friends() {
-    $("#itemlist-outgoing").empty();
+    $('#itemlist-outgoing').empty();
     for (i = 0; i < outgoing_friend_requests.length; i++) {
         newitem = $('<label class="item"></label>');
         newitem.text(outgoing_friend_requests[i]);
-        newitem.addClass("pending");
-        $("#itemlist-outgoing")
+        newitem.addClass('pending');
+        $('#itemlist-outgoing')
             .append(newitem);
     }
     if (outgoing_friend_requests.length === 0) {
         newitem = $('<div>No outgoing friend requests.</div>');
-        $("#itemlist-outgoing")
+        $('#itemlist-outgoing')
             .append(newitem);
     }
 }
+
 function set_incoming_friends() {
-    $("#itemlist-incoming").empty();
+    $('#itemlist-incoming')
+        .empty();
     for (i = 0; i < incoming_friend_requests.length; i++) {
         newitem = $('<label class="item"></label>');
         newitem.text(incoming_friend_requests[i]);
         newitem.data('username', incoming_friend_requests[i]);
-        var acceptButton = $('<input type="button" value="Accept"' +
-                             'class="item-button item-input-button"/>');
-        newitem.addClass("incoming");
+        var acceptButton = $('<input type="button" value="Accept"' + 'class="item-button item-input-button"/>');
+        newitem.addClass('incoming');
         newitem.append(acceptButton);
-        $("#itemlist-incoming").append(newitem);
-        newitem.find('input').on('click', incoming_button_callback);
+        $('#itemlist-incoming')
+            .append(newitem);
+        newitem.find('input')
+            .on('click', incoming_button_callback);
     }
     if (incoming_friend_requests.length === 0) {
         newitem = $('<div>No incoming friend requests.</div>');
-        $("#itemlist-incoming")
+        $('#itemlist-incoming')
             .append(newitem);
     }
 }
+
 function reloadFriends() {
     $.ajax({
-        type: "GET",
+        type: 'GET',
         url: base_url + '/get_friends',
         data: {
             uid: uid
         },
-        dataType: "json",
+        dataType: 'json',
         beforeSend: function() {
-            $("#itemlist").empty();
-            $("#itemlist").append($('<div class="loader">loading</div>'));
+            $('#itemlist')
+                .empty();
+            $('#itemlist')
+                .append($('<div class="loader">loading</div>'));
         },
         success: function(data) {
-            $("#itemlist").empty();
+            $('#itemlist')
+                .empty();
             friends = data;
             console.log(friends);
         },
@@ -105,18 +111,21 @@ function reloadFriends() {
         }
     });
     $.ajax({
-        type: "GET",
+        type: 'GET',
         url: base_url + '/get_outgoing_friend_reqs',
         data: {
             uid: uid
         },
-        dataType: "json",
+        dataType: 'json',
         beforeSend: function() {
-            $("#itemlist-outgoing").empty();
-            $("#itemlist-outgoing").append($('<div class="loader">loading</div>'));
+            $('#itemlist-outgoing')
+                .empty();
+            $('#itemlist-outgoing')
+                .append($('<div class="loader">loading</div>'));
         },
         success: function(data) {
-            $("#itemlist-outgoing").empty();
+            $('#itemlist-outgoing')
+                .empty();
             outgoing_friend_requests = data;
         },
         error: function(err) {
@@ -128,18 +137,21 @@ function reloadFriends() {
         }
     });
     $.ajax({
-        type: "GET",
+        type: 'GET',
         url: base_url + '/get_incoming_friend_reqs',
         data: {
             uid: uid
         },
-        dataType: "json",
+        dataType: 'json',
         beforeSend: function() {
-            $("#itemlist-incoming").empty();
-            $("#itemlist-incoming").append($('<div class="loader">loading</div>'));
+            $('#itemlist-incoming')
+                .empty();
+            $('#itemlist-incoming')
+                .append($('<div class="loader">loading</div>'));
         },
         success: function(data) {
-            $("#itemlist-incoming").empty();
+            $('#itemlist-incoming')
+                .empty();
             incoming_friend_requests = data;
         },
         error: function(err) {
@@ -154,12 +166,12 @@ function reloadFriends() {
 var friends;
 var outgoing_friend_requests;
 $.ajax({
-    type: "GET",
+    type: 'GET',
     url: base_url + '/get_username',
     data: {
         uid: uid
     },
-    dataType: "json",
+    dataType: 'json',
     success: function(data) {
         //alert(data);
         un_input.val(data);
@@ -168,13 +180,14 @@ $.ajax({
         newUserPopup();
     }
 });
+
 function removeFriend(friend_dom) {
     console.log(friend_dom);
     friend_name = friend_dom.text();
-    if (confirm("Do you want to remove " + friend_name + " from your friends?")) {
+    if (confirm('Do you want to remove ' + friend_name + ' from your friends?')) {
         //do it
         $.ajax({
-            type: "GET",
+            type: 'GET',
             url: base_url + '/delete_friend',
             data: {
                 uid: uid,
@@ -194,10 +207,11 @@ function removeFriend(friend_dom) {
         });
     }
 }
+
 function addFriendDom(friendName) {
     var newitem = $('<label class="item"></label>');
     newitem.text(friendName);
-    newitem.addClass("pending");
+    newitem.addClass('pending');
     reloadFriends();
 }
 $.fn.itemFriendList = function() {
@@ -217,40 +231,40 @@ $.fn.itemFriendList = function() {
         //$list.append($addButton);
     });
 };
-un_input = $("#un-input");
-un_input[0].addEventListener("input", function() {
+un_input = $('#un-input');
+un_input[0].addEventListener('input', function() {
     $.ajax({
-        type: "GET",
+        type: 'GET',
         url: base_url + '/username_in_use',
         data: {
             username: un_input.val(),
             uid: uid
         },
-        dataType: "json",
+        dataType: 'json',
         beforeSend: function() {
             //LOADING ICON
         },
         success: function(data) {
             console.log(data);
             if (data == '"False"') {
-                $("#border-target")
-                    .removeClass("bad");
-                $("#border-target")
-                    .addClass("good");
-                $("#un-label")
-                    .text("Username available!");
+                $('#border-target')
+                    .removeClass('bad');
+                $('#border-target')
+                    .addClass('good');
+                $('#un-label')
+                    .text('Username available!');
             } else {
-                $("#border-target")
-                    .removeClass("good");
-                $("#border-target")
-                    .addClass("bad");
-                $("#un-label")
-                    .text("Username already taken.");
+                $('#border-target')
+                    .removeClass('good');
+                $('#border-target')
+                    .addClass('bad');
+                $('#un-label')
+                    .text('Username already taken.');
             }
             //RED X, or GREEN CHECK, which will be erased when typing resumes
         },
         error: function(data) {
-            console.log("we lost");
+            console.log('we lost');
             console.log(data);
         },
         complete: function() {
@@ -264,44 +278,46 @@ un_input.keypress(function(e) {
         set_username();
     }
 });
-$("#friend-input")
+$('#friend-input')
     .keypress(function(e) {
         var key = e.which;
         if (key === 13) {
             friendClick($(this)[0].value, $(this));
         }
     });
-setButton = $("#set-btn");
-checkButton = $("#check-btn");
-friendButton = $("#friend-btn");
-set_username = function() {
+setButton = $('#set-btn');
+checkButton = $('#check-btn');
+friendButton = $('#friend-btn');
+
+function set_username() {
     uname = un_input.val();
     $.ajax({
-        type: "GET",
-        url: base_url + "/set_username",
+        type: 'GET',
+        url: base_url + '/set_username',
         data: {
             uid: uid,
             username: uname
         },
         success: function(data) {
-            if (data == "ok") {
-                alert("Name was successfully changed to " + uname + "!");
+            if (data == 'ok') {
+                alert('Name was successfully changed to ' + uname + '!');
             } else {
-                alert("Name was successfully set to " + uname + "!");
+                alert('Name was successfully set to ' + uname + '!');
             }
         },
         error: function(e) {
-            alert("Something went wrong!  Name was NOT set to " + uname + ".");
+            alert('Something went wrong!  Name was NOT set to ' + uname + '.');
         },
         complete: function() {
             //console.log("e");
         }
     });
 };
-setButton[0].addEventListener("click", set_username);
+setButton[0].addEventListener('click', set_username);
+
 function sendFriendRequest(fname, success, failure) {
     $.ajax({
-        type: "GET",
+        type: 'GET',
         url: base_url + '/send_friend_request',
         data: {
             uid: uid,
@@ -320,7 +336,7 @@ function sendFriendRequest(fname, success, failure) {
         complete: function() {}
     });
 }
-friendButton[0].addEventListener("click", function() {
+friendButton[0].addEventListener('click', function() {
     fname = $(this)
         .prev()
         .children()[0].value;
@@ -331,8 +347,7 @@ friendButton[0].addEventListener("click", function() {
         addFriendDom(fname);
         inpu.value = '';
     }, function() {
-        alert("Something went wrong—friend request not sent to " + fname + ".");
+        alert('Something went wrong—friend request not sent to ' + fname + '.');
     });
 });
-
 reloadFriends();
